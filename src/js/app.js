0bb9cc10b20bd13c8d5148f2edb35830286b4391
andreykorolevich <korolevich1994@mail.ru>
2020-10-07 09:14:45 +0300

    Changed protocol in URL

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import Widget from './Widget';
 
 const root = document.getElementById('root');
-const widget = new Widget(root, 'https://ws-backend-ajsb.herokuapp.com/ws');
+const widget = new Widget(root, 'wss://ws-backend-ajsb.herokuapp.com/ws');
 widget.createNick();
